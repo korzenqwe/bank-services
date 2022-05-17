a2a6003611480ed494085a2fb276615741c6cdae
korzenqwe <korzenqwe@gmail.com>
2022-05-17 23:40:43 +0300

    Fix MainPage

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <div className="workplace">
+      <h1>Главная страница</h1>
+    </div>
+  );
+};
+
+export default MainPage;
