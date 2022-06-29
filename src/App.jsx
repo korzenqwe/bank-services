cf88c2e899293bb5000db5819277044040b329ec
korzenqwe <korzenqwe@gmail.com>
2022-06-29 21:55:47 +0300

    feat: deleted route loan

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route, Link } from "react-router-dom";
 import "./styles/app.css";
 import MainPage from "./pages/MainPage";
 import Credit from "./pages/Credit";
-import Loan from "./pages/Loan";
 import Exchange from "./pages/Exchange";
 
 function App() {
@@ -12,14 +11,12 @@ function App() {
       <div className="navbar">
         <div className="navbar__links">
           <Link to="/credit">Кредитный калькулятор</Link>
-          <Link to="/loan">Калькулятор микрозаймов</Link>
           <Link to="/exchange">Конвертер валют</Link>
         </div>
       </div>
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
         <Route path="/credit" element={<Credit />}></Route>
-        <Route path="/loan" element={<Loan />}></Route>
         <Route path="/exchange" element={<Exchange />}></Route>
       </Routes>
     </>
